3eefefd11ea0d2fd10506ed285e09a7bd59ddd2a
fredcommo <fredcommo@gmail.com>
2023-03-17 16:48:21 +0100

    Minor changes in demo (timeout, ntrials)

--- forecast/demo.py
+++ forecast/demo.py
@@ -10,7 +10,7 @@ from forecast.utils import (
 from forecast.timeseries import TimeSeries
 
 def main():
-    path = "data"
+    path = "forecast/data"
     filename = 'jena_climate_2009_2016_simpl.csv'
     df = pd.read_csv(os.path.join(path, filename), parse_dates=["Date Time"])
 
@@ -23,9 +23,12 @@ def main():
     ts = TimeSeries(df, y='T (degC)', lags=72)
 
     # Optimize, then compute and plot predictions on test set
-    model_list=["LinearRegression", "Ridge", "Lasso", "ElasticNet"]
+    # model_list=["LinearRegression", "Ridge", "Lasso", "ElasticNet"]
+    model_list=["LinearRegression", "Ridge", "Lasso", "ElasticNet", "XGBReg"]
     # model_list=["XGBReg"]
-    ts.optimize(model_list=model_list, timeout=60, n_trials=10)
+
+    print("Testing:\n-", "\n- ".join(model_list))
+    ts.optimize(model_list=model_list, timeout=5*60, n_trials=20)
     ts.train_best_model()
     ts.plot()
 
