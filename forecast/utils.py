d3ecfffe7ce54d0e5fd5d67568d7de0dbc1d1f35
fredcommo <fredcommo@gmail.com>
2023-03-19 13:44:56 +0100

    Added utils to compute avg (daily, weekly, monthly, quarterly)

--- forecast/utils.py
+++ forecast/utils.py
@@ -37,3 +37,65 @@ def timestamp_to_yearly_sin_cos(timestamp_s):
     year = (365.2425)*day
     return np.sin(timestamp_s * (2 * np.pi / year)), \
             np.cos(timestamp_s * (2 * np.pi / year))
+
+def compute_daily_avg(df, date_col, y):
+    df_c = df.copy()
+    df_c["Day"] = df_c[date_col].dt.floor("D")
+
+    daily_avg = df_c \
+        .groupby(pd.Grouper(key=date_col, freq="D", closed='left', label='left'))[y] \
+        .mean() \
+        .reset_index(drop=False)
+    daily_avg.rename(columns={date_col: "Day", y: "Daily_avg"}, inplace=True)
+
+    df_c = df_c.merge(daily_avg, on=["Day"], how="left")
+    df_c.drop(columns=["Day"], inplace=True)
+
+    return df_c
+
+def compute_weekly_avg(df, date_col, y):
+    df_c = df.copy()
+    df_c["Week"] = df_c[date_col].dt.to_period("W").dt.start_time
+
+    weekly_avg = df_c \
+        .groupby(pd.Grouper(key=date_col, freq="W-MON", closed='left', label='left'))[y] \
+        .mean() \
+        .reset_index(drop=False)
+    weekly_avg[date_col] = weekly_avg[date_col].dt.to_period("W").dt.start_time
+    weekly_avg.rename(columns={date_col: "Week", y: "Weekly_avg"}, inplace=True)
+
+    df_c = df_c.merge(weekly_avg, on=["Week"], how="left")
+    df_c.drop(columns=["Week"], inplace=True)
+
+    return df_c
+
+def compute_monthly_avg(df, date_col, y):
+    df_c = df.copy()
+    df_c["Month"] = df_c[date_col].dt.to_period("M").dt.start_time
+
+    monthly_avg = df_c \
+        .groupby(pd.Grouper(key=date_col, freq="MS", closed='left', label='left'))[y] \
+        .mean() \
+        .reset_index(drop=False)
+    monthly_avg.rename(columns={date_col: "Month", y: "Monthly_avg"}, inplace=True)
+
+    df_c = df_c.merge(monthly_avg, on=["Month"], how="left")
+    df_c.drop(columns=["Month"], inplace=True)
+
+    return df_c
+
+def compute_quarterly_avg(df, date_col, y):
+    df_c = df.copy()
+    df_c["Quarter"] = df_c[date_col].dt.to_period("Q").dt.start_time
+
+    quaterly_avg = df_c \
+        .groupby(pd.Grouper(key=date_col, freq="QS", closed='left', label='left'))[y] \
+        .mean() \
+        .reset_index(drop=False)
+
+    quaterly_avg.rename(columns={date_col: "Quarter", y: "Quarterly_avg"}, inplace=True)
+
+    df_c = df_c.merge(quaterly_avg, on=["Quarter"], how="left")
+    df_c.drop(columns=["Quarter"], inplace=True)
+
+    return df_c
