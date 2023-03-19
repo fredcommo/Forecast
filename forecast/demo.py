1d6d3155361bc3294c485436749e44b5e19497f8
fredcommo <fredcommo@gmail.com>
2023-03-19 16:41:34 +0100

    Demo updated with daily, weekly avg computation

--- forecast/demo.py
+++ forecast/demo.py
@@ -4,7 +4,11 @@ import pandas as pd
 from forecast.utils import (
     date_to_timestamp,
     timestamp_to_daily_sin_cos,
-    timestamp_to_yearly_sin_cos
+    timestamp_to_yearly_sin_cos,
+    compute_daily_avg,
+    compute_weekly_avg,
+    compute_monthly_avg,
+    compute_quarterly_avg
     )
 
 from forecast.timeseries import TimeSeries
@@ -14,21 +18,43 @@ def main():
     filename = 'jena_climate_2009_2016_simpl.csv'
     df = pd.read_csv(os.path.join(path, filename), parse_dates=["Date Time"])
 
-    # turn dates into timestamps, then into sin/cos
+    #####################################################
+    # Compute daily, weekly, monthly, quarterly temp avg.
+    #####################################################
+    date_col = "Date Time"
+    y = "T (degC)"
+    df = compute_daily_avg(df, date_col=date_col, y=y)
+    df = compute_weekly_avg(df, date_col=date_col, y=y)
+    # df = compute_monthly_avg(df, date_col=date_col, y=y)
+    # df = compute_quarterly_avg(df, date_col=date_col, y=y)
+
+    #####################################################
+    # Turn dates into timestamps, then into sin/cos
+    #####################################################
     timestamp_s = date_to_timestamp(df, "Date Time")
     df['Day sin'], df['Day cos'] = timestamp_to_daily_sin_cos(timestamp_s)
     df['Year sin'], df['Year cos'] = timestamp_to_yearly_sin_cos(timestamp_s)
 
-    # Create a TimeSeries object
-    ts = TimeSeries(df, y='T (degC)', lags=48)
+    print(f"n rows: {df.shape[0]}")
+    print(f"n features: {df.shape[1] - 1}\n")
+    print(df.head())
 
+
+    #####################################################
+    # Create a TimeSeries object
+    #####################################################
+    ts = TimeSeries(df, y='T (degC)', lags=7*24)
+
+    #####################################################
     # Optimize, then compute and plot predictions on test set
-    model_list=["LinearRegression", "Ridge", "Lasso", "ElasticNet"]
-    # model_list=["LinearRegression", "Ridge", "Lasso", "ElasticNet", "XGBReg"]
+    #####################################################
     # model_list=["XGBReg"]
+    model_list=["Ridge", "Lasso", "ElasticNet"]
+    # model_list=["LinearRegression", "Ridge", "Lasso", "ElasticNet"]
+    # model_list=["LinearRegression", "Ridge", "Lasso", "ElasticNet", "XGBReg"]
 
     print("Testing:\n-", "\n- ".join(model_list))
-    ts.optimize(model_list=model_list, timeout=5*60, n_trials=30)
+    ts.optimize(model_list=model_list, timeout=20*60, n_trials=50)
     ts.train_best_model()
     ts.plot()
 
