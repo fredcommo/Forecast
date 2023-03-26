3b4c0aba0e98d89e2363cbe84f83d1190716c14c
fredcommo <fredcommo@gmail.com>
2023-03-26 09:14:01 +0200

    simplified demo

--- forecast/demo.py
+++ forecast/demo.py
@@ -4,6 +4,7 @@ import pandas as pd
 from forecast.utils import (
     date_to_timestamp,
     timestamp_to_daily_sin_cos,
+    timestamp_to_monthly_sin_cos,
     timestamp_to_yearly_sin_cos,
     compute_daily_avg,
     compute_weekly_avg,
@@ -21,8 +22,8 @@ def main():
     #####################################################
     # Compute daily, weekly, monthly, quarterly temp avg.
     #####################################################
-    # date_col = "Date Time"
-    # y = "T (degC)"
+    date_col = "Date Time"
+    y = "T (degC)"
     # df = compute_daily_avg(df, date_col=date_col, y=y)
     # df = compute_weekly_avg(df, date_col=date_col, y=y)
     # df = compute_monthly_avg(df, date_col=date_col, y=y)
@@ -33,7 +34,15 @@ def main():
     #####################################################
     timestamp_s = date_to_timestamp(df, "Date Time")
     df['Day sin'], df['Day cos'] = timestamp_to_daily_sin_cos(timestamp_s)
+    # df['Month sin'], df['Month cos'] = timestamp_to_monthly_sin_cos(timestamp_s)
     df['Year sin'], df['Year cos'] = timestamp_to_yearly_sin_cos(timestamp_s)
+
+    #####################################################
+    # Define the list of models to optimize
+    #####################################################
+    # Current available models: "LinearRegression", "XGBReg", "Ridge", "ElasticNet", "XGBReg"
+    model_list=["Lasso"]
+    print("Testing:\n-", "\n- ".join(model_list))
 
     #####################################################
     # Create a TimeSeries object
@@ -45,13 +54,35 @@ def main():
     #####################################################
     # Optimize, then compute and plot predictions on test set
     #####################################################
-    model_list=["ElasticNet", "Lasso", "Ridge"]
-    print("Testing:\n-", "\n- ".join(model_list))
 
-    ts.optimize(model_list=model_list, timeout=20*60, n_trials=50)
+    ts.optimize(model_list=model_list, timeout=2*60*60, n_trials=100)
     ts.train_best_model()
     ts.plot()
 
 
 if __name__ == "__main__":
     main()
+
+# Best XGBReg (quite slow)
+# {
+# "lags": 5*24,
+#  "scoring": "neg_mean_absolute_percentage_error",
+#  'learning_rate': 0.9,
+#  'n_estimators': 500,
+#  'reg_alpha': 0.3,
+#  'reg_lambda': 0.1
+# }
+
+# Best Ridge
+# {
+# "lags": 5*24,
+# 'alpha': 2.0,
+# 'positive': False
+# }
+
+# Best Lasso
+# {
+# "lags": 5*24,
+# 'alpha': 2.0,
+# 'positive': False
+# }
