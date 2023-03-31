84c14e4fbe7b931b387cfc41ef38f26677156e84
fredcommo <fredcommo@gmail.com>
2023-03-31 10:22:01 +0200

    Additional metrics available

--- forecast/metrics.py
+++ forecast/metrics.py
@@ -8,3 +8,27 @@ def wape(y, yhat):
         
     abs_diff = np.abs(y - yhat)
     return (1 - np.nansum(abs_diff) / np.nansum(y))
+
+def mape(y, yhat):
+    if any(y < 0):
+        m = np.nanmin(y)
+        y = y - m + 1
+        yhat = yhat - m + 1
+        
+    abs_diff = np.abs(y - yhat)
+    return (1 - np.nansum(abs_diff / y))
+
+def wmape(y, yhat):
+    if any(y < 0):
+        m = np.nanmin(y)
+        y = y - m + 1
+        yhat = yhat - m + 1
+
+    weights = np.array([y_i/np.nansum(y) for y_i in y])
+    return (1 - np.nansum(weights * np.abs(y - yhat) / y))
+
+
+def score(self, what="test", method="wape"):
+    y = self.get_ytest()
+    yhat = self.predict(what)
+    return eval(method)(y, yhat)
