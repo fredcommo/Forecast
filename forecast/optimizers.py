b10fe364203bc604d6d7a84316cb2228349e6595
fredcommo <fredcommo@gmail.com>
2023-03-31 10:22:49 +0200

    Updated gridsearch

--- forecast/optimizers.py
+++ forecast/optimizers.py
@@ -28,30 +28,36 @@ def LinearRegression_optimizer(trial):
 @optimizer(Ridge)
 def Ridge_optimizer(trial):
     params = {
-        "alpha": trial.suggest_float("ridge_alpha", 2, 100, step=2),
+        "alpha": trial.suggest_float("ridge_alpha", 2, 1000, step=2),
         "positive": trial.suggest_categorical("ridge_positive", [True, False]),
-        "solver": "auto"
+        "solver": "auto",
+        "max_iter": 20000,
+        "tol": 1e-4
         }
     return params
 
 @optimizer(Lasso)
 def Lasso_optimizer(trial):
     params = {
-        "alpha": trial.suggest_float("lasso_alpha", 2, 100, step=2),
+        "alpha": trial.suggest_float("lasso_alpha", 2, 1000, step=2),
         'warm_start': True, # trial.suggest_categorical("lasso_warm_start", [True, False]),
         "positive": trial.suggest_categorical("lasso_positive", [True, False]),
-        "selection": "cyclic" # trial.suggest_categorical("lasso_selection", ["cyclic", "random"])
+        "selection": trial.suggest_categorical("lasso_selection", ["cyclic", "random"]),
+        "max_iter": 1000000,
+        "tol": 1e-4
         }
     return params
 
 @optimizer(ElasticNet)
 def ElasticNet_optimizer(trial):
     params = {
-        "alpha": trial.suggest_float("elastic_alpha", 2, 100, step=2),
+        "alpha": trial.suggest_float("elastic_alpha", 2, 10000, step=2),
         "l1_ratio": trial.suggest_float("elastic_l1_ratio", 0.1, 1, step=0.1),
         'warm_start': True, # trial.suggest_categorical("elastic_warm_start", [True, False]),
         "positive": trial.suggest_categorical("elastic_positive", [True, False]),
-        "selection": "cyclic" # trial.suggest_categorical("elastic_selection", ["cyclic", "random"])
+        "selection": trial.suggest_categorical("elastic_selection", ["cyclic", "random"]),
+        "max_iter": 100000,
+        "tol": 1e-4
         }
     return params
 
@@ -61,7 +67,7 @@ def XGBReg_optimizer(trial):
         "learning_rate": 0.25, # trial.suggest_float("xgbr_learning_rate", 0.1, 1, step=0.1),
         "n_estimators": 500, # trial.suggest_categorical("xgbr_n_estimators", [100, 200, 500]),
         "booster": "gblinear",
-        "reg_alpha": trial.suggest_float("xgbr_reg_alpha", 0.1, 0.9, step=0.2),
-        "reg_lambda": trial.suggest_float("xgbr_reg_lambda", 0.1, 0.9, step=0.2)
+        "reg_alpha": trial.suggest_float("xgbr_reg_alpha", 0.1, 1, step=0.1),
+        "reg_lambda": trial.suggest_float("xgbr_reg_lambda", 0.1, 1, step=0.1)
         }
     return params
